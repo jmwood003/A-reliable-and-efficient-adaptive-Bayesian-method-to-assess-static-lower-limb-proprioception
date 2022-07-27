34726af6b1ce09d75e2ab06c8f596abbafc516c8
Jonathan Wood <47223948+jmwood003@users.noreply.github.com>
2022-07-27 08:33:11 -0400

    error button correction

--- Codes/Experiment/Functions/psi_lookupT.m
+++ Codes/Experiment/Functions/psi_lookupT.m
@@ -2,8 +2,8 @@ function [pr_left_x, pr_right_x] = psi_lookupT(X, alpha_range, beta_range)
 
 %Create a look up table for success and failure trials given both
 %parameters
-pr_left_x = nan(length(X),length(beta_range),length(alpha_range));
-pr_right_x = nan(length(X),length(beta_range),length(alpha_range));
+pr_left_x = nan(length(beta_range),length(alpha_range),length(X));
+pr_right_x = nan(length(beta_range),length(alpha_range),length(X));
 for x = 1:length(X)
     for a = 1:length(alpha_range)
         for b = 1:length(beta_range)
